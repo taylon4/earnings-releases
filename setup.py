078678b7a92f323b58fddbaa95e19278f63fe2f9
taylon4 <nstaylor03@gmail.com>
2023-10-06 14:51:50 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A package to scrape SEC data'
+LONG_DESCRIPTION = 'A package that allows developers to pull reports from publicly listed companies via the SEC'
+
+# Setting up
+setup(
+    name="sec_scrape",
+    version=VERSION,
+    author="Noah Taylor",
+    author_email="nstaylor03@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'sec', 'finance', 'requests'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
